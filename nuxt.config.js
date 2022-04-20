8d1c3e31f1c873d591a615027f81a04c41f9f214
shunsuke <shun.8000.sf@gmail.com>
2022-04-20 19:31:17 +0900

    [add] ファビコン追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,22 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'brueghel',
+    title: 'Brueghel | 画像変換ツール',
     htmlAttrs: {
       lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'Web制作者向けの画像変換サイトです。WebPへの変換にも対応しております。' },
+      { hid: 'keywords', name: 'keywords', content: 'web,jpeg,png,gif,webp,avif' },
+      { name: 'format-detection', content: 'telephone=no' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicons/icon-192x192.png' },
+      { rel: 'apple-touch-icon', type: 'image/png', href: '/favicons/apple-touch-icon-180x180.png' },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com'},
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ''},
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap'},
