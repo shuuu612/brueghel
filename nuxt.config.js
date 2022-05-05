96917401a7e4fd8d7244c5f78fd751abc019c948
shunsuke <shun.8000.sf@gmail.com>
2022-05-05 11:39:27 +0900

    [update] OGP追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,8 @@ export default {
   head: {
     title: 'Brueghel | 画像変換ツール',
     htmlAttrs: {
-      lang: 'ja'
+      lang: 'ja',
+      prefix: 'og: http://ogp.me/ns#'
     },
     meta: [
       { charset: 'utf-8' },
@@ -12,11 +13,21 @@ export default {
       { hid: 'keywords', name: 'keywords', content: 'web,jpeg,png,gif,webp,avif' },
       { name: 'format-detection', content: 'telephone=no' },
       { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+
+      // OGP設定
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Brueghel' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://brueghel.shuuu.jp/' },
+      { hid: 'og:title', property: 'og:title', content: 'Brueghel | 画像変換ツール' },
+      { hid: 'og:description', property: 'og:description', content: 'Web制作者向けの画像変換サイトです。WebPへの変換にも対応しております。' },
+      { hid: 'og:image', property: 'og:image', content: 'https://brueghel.shuuu.jp/ogp.png' },
+
+      // Twitter
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@shuuu612' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'icon', type: 'image/png', href: '/favicons/icon-192x192.png' },
-      { rel: 'apple-touch-icon', type: 'image/png', href: '/favicons/apple-touch-icon-180x180.png' },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com'},
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ''},
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap'},
